0ec720562176895c4361068d6a7a35952a080181
San Han <sn1994a@gmail.com>
2025-02-12 12:52:45 +0100

    add links to publications

--- generate_html.py
+++ generate_html.py
@@ -13,10 +13,12 @@ with open("profile/template.html", "r", encoding="utf-8") as f:
     HTML_TEMPLATE = f.read()
 
 def fetch_ads_papers(query):
-    papers = ads.SearchQuery(q=query, sort="date desc", rows=paper_limit, fl=['title', 'author', 'pubdate', 'pub', 'bibcode', 'volume', 'page', 'bibstem'])
+    papers = ads.SearchQuery(q=query, sort="date desc", rows=paper_limit, fl=['title', 'author', 'pubdate', 'pub', 'bibcode', 'volume', 'page', 'bibstem', 'doi', 'identifier'])
     results = []
     for paper in papers:
         author_mod = [author_name_format(author) for author in paper.author]
+        arxiv_id = next((id for id in paper.identifier if id.startswith("arXiv:")), None)
+        arxiv_id_clean = arxiv_id.split(":")[-1] if arxiv_id else None
         for i, author in enumerate(paper.author):
             if author == my_name:
                 author_mod[i] = "<b>%s</b>" % author_mod[i]
@@ -25,7 +27,13 @@ def fetch_ads_papers(query):
             "Authors": "; ".join(author_mod[:author_limit]) + ("; ..." if len(author_mod) > author_limit else ""),
             "Details": f"{paper.pubdate[:4]}, <b>{paper.bibstem[0]}</b>, {paper.volume}, {paper.page[0]}" if paper.volume else f"{paper.pubdate[:4]}, <b>{paper.bibstem[0]}</b>",
             "Bibcode": paper.bibcode if paper.bibcode else "#",
+            "ads": f"<a href='https://ui.adsabs.harvard.edu/abs/{paper.bibcode}'><small>[ADS]</small></a>" if paper.bibcode else "",
+            "doi": f"<a href='https://doi.org/{paper.doi[0]}'><small>[DOI]</small></a>" if paper.doi else "",
+            "arxiv": f"<a href='https://arxiv.org/abs/{arxiv_id_clean}'><small>[arXiv]</small></a>" if arxiv_id_clean else "",
+            "html": f"<a href='https://ar5iv.labs.arxiv.org/html/{arxiv_id_clean}'><small>[HTML]</small></a>" if arxiv_id_clean else "",
         })
+
+        
     return results
 
 def write_html(query, output_file="papers.html"):
@@ -33,12 +41,12 @@ def write_html(query, output_file="papers.html"):
     
     # Generate HTML content
     html_content = HTML_TEMPLATE
-    papers_html = ""
+    num_papers = len(papers)
     for paper in papers:
         papers_html += f"""
         <p>
-            <a href="https://ui.adsabs.harvard.edu/abs/{paper['Bibcode']}">{paper['Title']}</a><br>
-            {paper['Authors']}, {paper['Details']}
+            <em>{paper['Title']}</em><br>
+            {paper['Authors']}, {paper['Details']} {paper['ads']} {paper['doi']} {paper['arxiv']} {paper['html']} 
         </p>
         """
     html_content = html_content.replace("{% for paper in papers %}", papers_html).replace("{% endfor %}", "")
