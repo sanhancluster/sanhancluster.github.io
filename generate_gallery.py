748cddedd8fcd9a83e07ba918ba3cf3407b759c3
San Han <sn1994a@gmail.com>
2025-07-28 18:20:09 +0200

    Add zoomable images in gallery

--- generate_gallery.py
+++ generate_gallery.py
@@ -3,19 +3,21 @@ from PIL import Image
 
 # 설정
 source_dir = "./profile/gallery"
-thumb_dir = "./profile/thumbnails"
-thumb_size = 600
+large_dir = "./profile/gallery_large"
+thumb_dir = "./thumbnails"
+thumb_size = 400
 output_file = "./profile/gallery.html"
 
 os.makedirs(thumb_dir, exist_ok=True)
 
-valid_images = ('.png', '.jpg', '.webp')
+valid_images = ('.png', '.jpg', '.webp', 'dzi', '.avif')
+valid_zoomable = ('dzi')
 valid_videos = ('.mp4', '.webm')
 
 with open("profile/gallery_template.html", "r", encoding="utf-8") as f:
     HTML_TEMPLATE = f.read()
-
 html_content = HTML_TEMPLATE
+
 #num_papers = len(papers)
 images_html = "<h2>Images</h2>"
 Image.MAX_IMAGE_PIXELS = None
@@ -33,12 +35,11 @@ for filename in os.listdir(source_dir):
     if filename.lower().endswith(valid_images):
         source_path = os.path.join(source_dir, filename)
         name, ext = os.path.splitext(filename)
-        thumb_path = os.path.join(thumb_dir, f"{name}_thumb.webp")
+        thumb_path = os.path.join(source_dir, thumb_dir, f"{name}_thumb.webp")
+        os.makedirs(os.path.dirname(thumb_path), exist_ok=True)
         try:
             with Image.open(source_path) as img:
                 img = img.convert("RGBA")
-                #aspect_ratio = img.width / img.height
-                #new_width = int(thumb_height * aspect_ratio)
                 square_img = crop_center_square(img)
                 resized = square_img.resize((thumb_size, thumb_size), Image.LANCZOS)
                 resized.save(thumb_path, "WEBP")
@@ -48,10 +49,22 @@ for filename in os.listdir(source_dir):
         source_path = os.path.relpath(source_path, start=os.path.dirname(output_file))
         thumb_path = os.path.relpath(thumb_path, start=os.path.dirname(output_file))
         images_html += f"""
-        <a href="{source_path}"><img src="{thumb_path}" alt="{name}" height="400px"></a>
+        <a href="{source_path}"><img src="{thumb_path}" alt="{name}" height="{thumb_size}px"></a>
         """
-
 html_content = html_content.replace("{list_images}", images_html)
+
+large_html = ""
+for filename in os.listdir(large_dir):
+    if filename.lower().endswith(valid_zoomable):
+        source_path = os.path.join(large_dir, filename)
+        name, ext = os.path.splitext(filename)
+        thumb_path = os.path.join(large_dir, thumb_dir, name+"_thumb.webp")
+        source_path = os.path.relpath(source_path, start=os.path.dirname(output_file))
+        thumb_path = os.path.relpath(thumb_path, start=os.path.dirname(output_file))
+        large_html += f"""
+        <img src="{thumb_path}" data-dzi="{source_path}" alt="{name}" height="{thumb_size}px">
+        """
+html_content = html_content.replace("{list_large_images}", large_html)
 
 videos_html = "<h2>Videos</h2>"
 for filename in os.listdir(source_dir):
